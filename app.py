4e49c509df5bb7f6b37cd9f91be878682ec3e8c4
syedaymen <emailtoaymen@gmail.com>
2023-08-20 15:26:18 +0530

    deployed backened

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9935b765e9d4070a0712ddb8f508098f221e399a293721c4739aa0d7bc9cb493
-size 431
+import pickle
+from fastapi import FastAPI
+app = FastAPI()
+
+model = pickle.load(open('aiseatpredictor_decisionTree.pkl', 'rb'))
+
+
+@app.get("/{rank}/{caste}")
+def read_root(rank: int, caste: int):
+    global model
+    predictions = model.predict([[rank, caste]])
+    return {"college": predictions[0][0], "branch": predictions[0][1]}
